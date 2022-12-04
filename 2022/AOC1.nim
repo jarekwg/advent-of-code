b160b767c5ccd90b2cde6305b9940206246bcbf0
Jarek Głowacki <jarekwg@gmail.com>
2022-12-04 23:51:20 +1100

    Adjust alternative, so that it's parallel to the uncommented path.

--- 2022/AOC1.nim
+++ 2022/AOC1.nim
@@ -7,6 +7,6 @@ echo "Part 1: ", elfloads.max
 echo "Part 2: ", elfloads.sorted[^3..^1].sum
 
 # # ALTERNATIVE SYNTAX -- not sure I like that this is possible
-# let elfloads = sorted(mapIt(split(strip(readFile("1")), "\n\n"), sum(map(split(it), parseInt))))
-# echo "Part 1: ", elfloads[^1]
-# echo "Part 2: ", sum(elfloads[^3..^1])
+# let elfloads = mapIt(split(strip(readFile("1")), "\n\n"), sum(map(split(it), parseInt)))
+# echo "Part 1: ", max(elfloads)
+# echo "Part 2: ", sum(sorted(elfloads)[^3..^1])
